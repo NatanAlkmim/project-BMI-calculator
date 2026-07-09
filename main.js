3e38f6bf81c54b522329ede1ec5b79d1cd304d40
NatanAlkmim <natan.alkmim@gmail.com>
2026-07-09 10:52:14 -0300

    Start BMI calculator project

---
+++ main.js
@@ -0,0 +1,62 @@
+// IMC = peso (kg) ÷ (altura (m) × altura (m))
+
+/*
+
+
+Passo 2
+  1 -> começar sem o prompt e depois evoluir
+
+*/
+
+function userInfo() {
+    let peso;
+    let imputPeso;
+
+  do {
+
+    peso = prompt("Digite o seu peso");
+
+    if (peso === null) break;
+
+    peso = peso.replace(",", ".");
+    imputPeso = Number(peso);
+
+    if (imputPeso <= 0) alert("Coloque um número maior que zero");
+    else if (Number.isNaN(imputPeso)) alert("Precisa ser um número maior que zero");
+
+  } while (imputPeso <= 0 || Number.isNaN(imputPeso));
+
+  let altura;
+  let imputAltura;
+
+  do {
+
+    altura = prompt("Digite a sua altura");
+
+    if (altura === null) break;
+    
+    altura = altura.replace(",", ".");
+    imputAltura = Number(altura);
+
+    if (imputAltura <= 0) alert("Coloque uma altura maior que zero");
+    else if (Number.isNaN(imputAltura)) alert("Precisa ser um número maior que zero");
+
+  } while (imputAltura <= 0 || Number.isNaN(imputAltura));
+
+  return { peso: imputPeso, altura: imputAltura};
+}
+const resultado = userInfo()
+
+
+function imc(peso, altura) {
+  const calculoIMC = Number((peso / Math.pow(altura, 2)).toFixed(2));
+  
+  if (Number.isNaN(calculoIMC)) return;
+
+  if (calculoIMC <= 18.5) alert(`Seu IMC: ${calculoIMC}\nAbaixo do peso`);
+  else if (calculoIMC <= 24.9) alert(`Seu IMC: ${calculoIMC}\nPeso normal`);
+  else if (calculoIMC <= 29.9) alert(`Seu IMC: ${calculoIMC}\nSobrepeso`);
+  else alert(`Seu IMC: ${calculoIMC}\nObesidade`);
+}
+
+imc(resultado.peso, resultado.altura);
